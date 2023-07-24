0ac1ecb6527f82b0ae244e7d26d3cc4a8c6f84c5
Intuneteq <tobiolanitori@gmail.com>
2023-07-24 16:23:27 +0100

    feat: auth implemented on the backend

--- react/src/contexts/AppContext.tsx
+++ react/src/contexts/AppContext.tsx
@@ -208,7 +208,7 @@ export const initState: AppState = {
         email: "tom@example.com",
         avatar: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
-    token: "123",
+    token: "",
     surveys: [...TMP_SURVEY],
 };
 
--- react/src/pages/auth/Register.tsx
+++ react/src/pages/auth/Register.tsx
@@ -1,12 +1,38 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const Register = () => {
+    const [fullName, setFullName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    
     return (
         <>
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
+                Sign up to your account
             </h2>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form className="space-y-6" action="#" method="POST">
+                    <div>
+                        <label
+                            htmlFor="full_name"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                            Full Name
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="full_name"
+                                name="name"
+                                type="text"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
                     <div>
                         <label
                             htmlFor="email"
@@ -34,14 +60,6 @@ const Register = () => {
                             >
                                 Password
                             </label>
-                            <div className="text-sm">
-                                <a
-                                    href="#"
-                                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                                >
-                                    Forgot password?
-                                </a>
-                            </div>
                         </div>
                         <div className="mt-2">
                             <input
@@ -49,6 +67,26 @@ const Register = () => {
                                 name="password"
                                 type="password"
                                 autoComplete="current-password"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label
+                                htmlFor="confirm_password"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                            >
+                                Confirm Password
+                            </label>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="confirm_password"
+                                name="confirm_password"
+                                type="password"
                                 required
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
@@ -66,13 +104,13 @@ const Register = () => {
                 </form>
 
                 <p className="mt-10 text-center text-sm text-gray-500">
-                    Not a member?{" "}
-                    <a
-                        href="#"
+                    Already have an account?{" "}
+                    <Link
+                        to={'/login'}
                         className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
                     >
-                        Start a 14 day free trial
-                    </a>
+                        Login
+                    </Link>
                 </p>
             </div>
         </>
